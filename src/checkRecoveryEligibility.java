2906ee81cb91b50e6f50af567087d594a8c7c6bd
Charl <151132997+Charldotpy@users.noreply.github.com>
2025-11-25 11:31:31 +0800

    Update checkRecoveryEligibility.java

--- src/checkRecoveryEligibility.java
+++ src/checkRecoveryEligibility.java
@@ -249,6 +249,7 @@ public class checkRecoveryEligibility extends JFrame {
 
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(STUDENT_INFO)))
         {
+            bw.write("StudentID, FirstName, LastName, ProgramID, Email, RecoveryEligibility\n");
             for (String[] student : students)
             {
                 String line = String.join(",", student);
@@ -267,3 +268,4 @@ public class checkRecoveryEligibility extends JFrame {
         new checkRecoveryEligibility();
     }
 }
+
